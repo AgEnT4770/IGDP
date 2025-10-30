1f9cf05c5f7ff309db6c12017c937a6ed58134ae
Mohamed Ibrahim <mohamed.ibrahimturkey@gmail.com>
2025-10-30 18:31:05 +0300

    commit some changes to pull

--- app/src/main/java/com/example/igdp/GamePage.kt
+++ app/src/main/java/com/example/igdp/GamePage.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.igdp.ui.theme.IGDPTheme
 
 
@@ -29,7 +31,11 @@ class GamePageActivity : ComponentActivity() {
 
 @Composable
 fun GameDetails(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.padding(horizontal = 24.dp)
+    ){
 
+    }
 }
 
 
