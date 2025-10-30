7559f1089ce07711fc0691a90d82a844d485432c
Mohamed Ibrahim <mohamed.ibrahimturkey@gmail.com>
2025-10-30 18:15:25 +0300

    save changes to pull...

--- app/src/main/java/com/example/igdp/GamePage.kt
+++ app/src/main/java/com/example/igdp/GamePage.kt
@@ -1,9 +1,42 @@
 package com.example.igdp
 
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.igdp.ui.theme.IGDPTheme
+
+
+class GamePageActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            IGDPTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    GameDetails(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
 
 @Composable
-fun GamePage(modifier: Modifier = Modifier) {
-    // TODO: Implement the game page
+fun GameDetails(modifier: Modifier = Modifier) {
+
+}
+
+
+@Preview(showBackground = false, showSystemUi = true)
+@Composable
+private fun GameDetailsPreview() {
+    IGDPTheme {
+        GameDetails()
+    }
 }
